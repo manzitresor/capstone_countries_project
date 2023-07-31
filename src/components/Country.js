c08b05175466366a0c1a97086cad7673ec6ddf92
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-31 14:43:32 +0200

    add fetchdata from PI

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,22 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCountries } from '../redux/countries/countriesSlice';
 
 function Country() {
+  const { countries, isLoading, error } = useSelector((state) => state.country);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
+  if (isLoading === true) {
+    console.log('Looding.....');
+  }
+  if (error) {
+    console.log(error);
+  }
+  console.log(countries);
   return (
     <>
       <h1>Country</h1>
---
+++ src/redux/countries/countriesSlice.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://restcountries.com/v3.1/all?fields=name,flags';
+
+export const fetchCountries = createAsyncThunk('countries/fetchCountries', async (thunkAPI) => {
+  try {
+    const resp = await axios.get(url);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('ooops smoething wrent wrong');
+  }
+});
+const initialState = {
+  countries: [],
+  isLoading: false,
+  error: null,
+};
+
+const countriesSlice = createSlice({
+  name: 'Countries',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCountries.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchCountries.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.countries = action.payload;
+    });
+
+    builder.addCase(fetchCountries.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+export default countriesSlice.reducer;
